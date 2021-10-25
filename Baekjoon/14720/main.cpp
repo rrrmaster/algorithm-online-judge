c3f871b579b1cc1d0743200cfc5e4d18e4a71f91
고수처럼 <dudgus9101@gmail.com>
2021-10-25 12:53:37 +0900

    Create main.cpp

---
+++ Baekjoon/14720/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+int solved(std::vector<int>& v, int n)
+{
+	int k = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (k%3 == (v[i] % 3))
+		{
+			k += 1;
+		}
+	}
+	return k;
+}
+
+int main()
+{
+	int n;
+	std::vector<int> v;
+	std::cin >> n;
+	v.resize(n);
+	for (int i = 0; i < n; i++)
+	{
+		std::cin >> v[i];
+	}
+	std::cout << solved(v, n);
+}
